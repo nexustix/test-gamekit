7a49e7f39c09bdf9f49baf82e1fffc1ebdb4d80f
nexustix <nexus1768@tuta.io>
2018-04-22 20:07:42 +0200

    Add draw call

--- test-gamekit.lisp
+++ test-gamekit.lisp
@@ -7,7 +7,14 @@
 (defvar *canvas-width* 800)
 (defvar *canvas-height* 600)
 
+(defvar *black* (gamekit:vec4 0 0 0 1))
+(defvar *origin* (gamekit:vec2 0 0))
+
 (gamekit:defgame test-gamekit () ()
   (:viewport-width *canvas-width*)
   (:viewport-height *canvas-height*)
   (:viewport-title "Testing Gamekit!"))
+
+
+(defmethod gamekit:draw ((app test-gamekit))
+  (gamekit:draw-rect *origin* 100 100 :fill-paint *black*))
